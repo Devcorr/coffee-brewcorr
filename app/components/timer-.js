4179f8f04cb222567b6c48dcab5ce748cb477965
Matt Bargar <matt@devcorr.com>
2015-07-07 21:22:23 -0400

    starting to rough out timer logic and UI

--- app/components/timer-.js
+++ app/components/timer-.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  duration: 0,
+  timer: null,
+
+  start: function () {
+    // use ember.run.later to schedule a timer update and save the timer in this.timer
+    // have the callback call this function as its last statement
+  },
+
+  stop: function () {
+    // use ember.run.cancel to stop the timer in the timer object
+  },
+
+  actions: {
+    startTimer: function () {
+
+    },
+
+    stopTimer: function () {
+
+    }
+  }
 });
